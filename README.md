4051c5d91cc1fcc9fd47f5ffd019b8b6a31b94bd
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-08-10 12:41:19 -0400

    Add boilerplate from nektool
    Implements a basic python package with CLI for executing nextflow
    workflows.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# CCBR_NextflowPipelineCookiecutter
-CCBR Template for Nextflow 
+# Nextflow Template
+
+CCBR template for creating Nextflow pipelines
+
+This template takes inspiration from nektool[^nektool https://github.com/beardymcjohnface/nektool] and the nf-core template. If you plan to submit your pipeline to nf-core, don't use this template -- instead follow their instructions[^create nf-core pipelines https://nf-co.re/docs/contributing/tutorials/creating_with_nf_core].
+
+## Getting started
+
+1. On GitHub, click "Use this template" and "Create a new repository".
+1. Choose an owner and repository name.
+1. Change all instances of `TOOL_NAME` throughout the repo with the actual tool name. Places include: 
+  - `README.md`
+  - `pyproject.toml`
+  - `src/TOOL_NAME`
+  - `src/TOOL_NAME/CITATION`
+  - `src/TOOL_NAME/__main__.py`
+1. Edit `pyproject.toml` with correct information for your tool. You will likely need to change:
+  - author names and emails
+  - dependencies
+  - project URLs
+1. Write your nextflow workflow in `src/TOOL_NAME/workflow`.
+1. Write your documentation in `docs/`.
+
+You can look for instances of `TOOL_NAME` in case you missed any with grep:
+```sh
+grep -r "TOOL_NAME" *
+```
+
+## Usage
+
+Install the tool in edit mode:
+```sh
+pip3 install -e . 
+```
+
+Run the example
+```sh
+TOOL_NAME run --input "Hello world"
+```
+
+## Maintaining your tool
+
+- Increment the [version](src/TOOL_NAME/VERSION) number following [semantic versioning guidelines](https://semver.org/).
+- Keep the [changelog](CHANGELOG.md) up to date with any user-facing changes.
---
+++ src/TOOL_NAME/util.py
@@ -0,0 +1,165 @@
+"""MISC FUNCTIONS
+You shouldn't need to tweak these much if at all
+"""
+
+import sys
+import os
+import subprocess
+import yaml
+import collections.abc
+from shutil import copyfile
+from time import localtime, strftime
+
+import click
+
+
+def nek_base(rel_path):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), rel_path)
+
+
+def get_version():
+    with open(nek_base("VERSION"), "r") as f:
+        version = f.readline()
+    return version
+
+
+def print_citation():
+    with open(nek_base("CITATION"), "r") as f:
+        for line in f:
+            click.echo(line, nl=False, err=True)
+
+
+def msg(err_message):
+    tstamp = strftime("[%Y:%m:%d %H:%M:%S] ", localtime())
+    click.echo(tstamp + err_message, err=True)
+
+
+def msg_box(splash, errmsg=None):
+    msg("-" * (len(splash) + 4))
+    msg(f"| {splash} |")
+    msg(("-" * (len(splash) + 4)))
+    if errmsg:
+        click.echo("\n" + errmsg, err=True)
+
+
+def append_config_block(nf_config="nextflow.config", scope=None, **kwargs):
+    with open(nf_config, "a") as f:
+        f.write(scope.rstrip() + "{" + "\n")
+        for k in kwargs:
+            f.write(f"{k} = {kwargs[k]}\n")
+        f.write("}\n")
+
+
+def copy_config(local_config=None, system_config=None):
+    msg(f"Copying system default config to {local_config}")
+    copyfile(system_config, local_config)
+
+
+def read_config(file):
+    with open(file, "r") as stream:
+        _config = yaml.safe_load(stream)
+    return _config
+
+
+def update_config(config, overwrite_config):
+    def _update(d, u):
+        for (key, value) in u.items():
+            if isinstance(value, collections.abc.Mapping):
+                d[key] = _update(d.get(key, {}), value)
+            else:
+                d[key] = value
+        return d
+    _update(config, overwrite_config)
+
+
+def write_config(_config, file):
+    msg(f"Writing runtime config file to {file}")
+    with open(file, "w") as stream:
+        yaml.dump(_config, stream)
+
+
+class OrderedCommands(click.Group):
+    """Preserve the order of subcommands when printing --help"""
+
+    def list_commands(self, ctx: click.Context):
+        return list(self.commands)
+
+
+"""RUN A NEXTFLOW FILE
+Hopefully you shouldn't need to tweak this function at all.
+- You must provide a Nextflow file, all else is optional
+- Highly recommend supplying a params file and a config file"""
+
+
+def run_nextflow(
+    paramsfile=None,
+    configfile=None,
+    nextfile_path=None,
+    merge_config=None,
+    threads=None,
+    use_conda=False,
+    conda_frontend=None,
+    conda_prefix=None,
+    nextflow_args=None,
+):
+    """Run a Nextflow workfile"""
+    nextflow_command = ["nextflow", "run", nextfile_path]
+
+    if paramsfile:
+        # copy sys default params if needed
+        copy_config(
+            local_config=paramsfile,
+            system_config=nek_base(os.path.join("workflow", "params.yaml")),
+        )
+
+        # read the params
+        nf_config = read_config(paramsfile)
+
+        # merge in command line params if provided
+        if merge_config:
+            update_config(nf_config, merge_config)
+
+        # update params file
+        write_config(nf_config, paramsfile)
+        nextflow_command += ["-params-file", paramsfile]
+
+        # display the runtime params
+        msg_box("Runtime parameters", errmsg=yaml.dump(nf_config, Dumper=yaml.Dumper))
+
+    if configfile:
+        copy_config(
+            local_config=configfile,
+            system_config=nek_base(os.path.join("workflow", "nextflow.config")),
+        )
+
+        # add threads
+        if threads:
+            append_config_block(scope="executor", cpus=threads)
+
+        # Use conda
+        if use_conda:
+            if conda_frontend == "mamba":
+                append_config_block(
+                    scope="conda", useMamba='"true"', cacheDir=f'"{conda_prefix}"'
+                )
+            else:
+                append_config_block(scope="conda", cacheDir=f'"{conda_prefix}"')
+
+        nextflow_command += ["-c", configfile]
+
+        # display the runtime configuration
+        msg_box("Launcher Configuration", errmsg=open(configfile, "r").read())
+
+    # add any additional Nextflow commands
+    if nextflow_args:
+        nextflow_command += list(nextflow_args)
+
+    # Run Nextflow!!!
+    nextflow_command = " ".join(str(nf) for nf in nextflow_command)
+    msg_box("Nextflow command", errmsg=nextflow_command)
+    if not subprocess.run(nextflow_command, shell=True).returncode == 0:
+        msg("Error: Nextflow failed")
+        sys.exit(1)
+    else:
+        msg("Nextflow finished successfully")
+    return 0
