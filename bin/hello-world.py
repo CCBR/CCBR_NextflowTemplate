d2b2f48ebb6db13df752112e90ac922b9cebed0a
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-09-13 11:53:16 -0400

    feat: create python script for bin/

---
+++ bin/hello-world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+Example script that could be used by a nextflow process
+"""
+
+import sys
+
+
+def main():
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
