d749c95706d9a8658b30b06217d046d1ce28fd33
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-11-15 14:48:27 -0500

    refactor: update CLI from CHAMPAGNE
    resolves #17

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,5 @@
 """
-Entrypoint for pipeline CLI
+Entrypoint for TOOL_NAME CLI
 
 Check out the wiki for a detailed look at customizing this file:
 https://github.com/beardymcjohnface/Snaketool/wiki/Customising-your-Snaketool
@@ -20,21 +20,6 @@ from .util import (
 def common_options(func):
     """Common options decorator for use with click commands."""
     options = [
-        click.option(
-            "--configfile",
-            default="nextflow.config",
-            help="Custom config file",
-            show_default=True,
-        ),
-        click.option(
-            "--paramsfile", default=None, help="Custom params file", show_default=True
-        ),
-        click.option(  # when threads=None, uses max available
-            "--threads",
-            help="Number of threads to use",
-            default=None,
-            show_default=True,
-        ),
         click.argument("nextflow_args", nargs=-1),
     ]
     for option in reversed(options):
@@ -47,7 +32,7 @@ def common_options(func):
 )
 @click.version_option(get_version(), "-v", "--version", is_flag=True)
 def cli():
-    """TOOL_NAME description TODO
+    """CHromAtin iMmuno PrecipitAtion sequencinG aNalysis pipEline
 
     For more options, run:
     tool_name [command] --help"""
@@ -56,15 +41,17 @@ def cli():
 
 help_msg_extra = """
 \b
-CLUSTER EXECUTION:
-tool_name run ... -profile [profile],[profile],...
-For information on Nextflow config and profiles see:
-https://www.nextflow.io/docs/latest/config.html#config-profiles
-\b
-RUN EXAMPLES:
-Use singularity:    tool_name run ... -profile singularity
-Specify threads:    tool_name run ... --threads [threads]
-Add NextFlow args:  tool_name run ... -work-dir workDir -with-docker
+EXAMPLES:
+Execute with slurm:
+    tool_name run ... --mode slurm
+Preview the processes that will run:
+    tool_name run ... --mode local -preview
+Add nextflow args (anything supported by `nextflow run`):
+    tool_name run ... -work-dir path/to/workDir
+Run with a specific installation of tool_name:
+    tool_name run --main path/to/tool_name/main.nf ...
+Run with a specific tag, branch, or commit from GitHub:
+    tool_name run --main CCBR/TOOL_NAME -r v0.1.0 ...
 """
 
 
@@ -74,32 +61,48 @@ Add NextFlow args:  tool_name run ... -work-dir workDir -with-docker
         help_option_names=["-h", "--help"], ignore_unknown_options=True
     ),
 )
+@click.option(
+    "--main",
+    "main_path",
+    help="Path to the tool_name main.nf file or the GitHub repo (CCBR/TOOL_NAME). Defaults to the version installed in the $PATH.",
+    type=str,
+    default=nek_base(os.path.join("main.nf")),
+    show_default=True,
+)
+@click.option(
+    "--mode",
+    "_mode",
+    help="Run mode (slurm, local)",
+    type=str,
+    default="local",
+    show_default=True,
+)
 @common_options
-def run(**kwargs):
+def run(main_path, _mode, **kwargs):
     """Run the workflow"""
-    # optional: merge config from CLI with nf config
-    # run!
+    if (  # this is the only acceptable github repo option for tool_name
+        main_path != "CCBR/TOOL_NAME"
+    ):
+        # make sure the path exists
+        if not os.path.exists(main_path):
+            raise FileNotFoundError(
+                f"Path to the tool_name main.nf file not found: {main_path}"
+            )
+
     run_nextflow(
-        nextfile_path=nek_base(os.path.join("main.nf")),  # Full path to Nextflow file
+        nextfile_path=main_path,
+        mode=_mode,
         **kwargs,
     )
 
 
 @click.command()
-@click.option(
-    "--configfile",
-    default="nextflow.config",
-    help="Copy template config to file",
-    show_default=True,
-)
-def config(configfile, **kwargs):
-    """Copy the system default config files"""
-    for filename in ("nextflow.config", "params.yml"):
-        if os.path.exists(nek_base(filename)):
-            copy_config(
-                local_config=configfile,
-                system_config=nek_base(filename),
-            )
+def init(**kwargs):
+    """Initialize the working directory by copying the system default config files"""
+    paths = ("nextflow.config", "conf/", "assets/")
+    copy_config(paths)
+    if not os.path.exists("log/"):
+        os.mkdir("log/")
 
 
 @click.command()
@@ -109,7 +112,7 @@ def citation(**kwargs):
 
 
 cli.add_command(run)
-cli.add_command(config)
+cli.add_command(init)
 # cli.add_command(citation) # TODO uncomment if tool_name is published in a journal or Zenodo
 
 
