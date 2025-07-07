4b5404766da694c8b04c497ff545a222f706e94c
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-07-07 11:36:37 -0400

    chore: update spooker cmd

--- lib/Utils.groovy
+++ lib/Utils.groovy
@@ -2,14 +2,14 @@ class Utils {
     // run spooker for the workflow
     public static String spooker(workflow) {
         def pipeline_name = "${workflow.manifest.name.tokenize('/')[-1]}"
-        def command_string = "spooker ${workflow.launchDir} ${pipeline_name}"
         def out = new StringBuilder()
         def err = new StringBuilder()
         def spooker_in_path = check_command_in_path("spooker")
         if (spooker_in_path) {
             try {
                 println "Running spooker"
-                def command = command_string.execute()
+                def spooker_command = "spooker --outdir ${workflow.launchDir} --name ${pipeline_name} --version ${workflow.manifest.version} --path ${workflow.projectDir}"
+                def command = spooker_command.execute()
                 command.consumeProcessOutput(out, err)
                 command.waitFor()
             } catch(IOException e) {
